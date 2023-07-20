7df9f96784a032f68285023685e403a82a1d7675
kanyousyokubutu <68042170+Luibosutini@users.noreply.github.com>
2023-07-21 07:33:32 +0900

    Add files via upload

---
+++ aout_putter.py
@@ -0,0 +1,137 @@
+import sys
+import requests
+import json
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QCheckBox, QListWidget, QMessageBox
+
+
+class MemoApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.memo_list = []
+        self.tag_list = []
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QVBoxLayout()
+
+        self.login_layout = QHBoxLayout()
+        self.login_label = QLabel("Qiitaアクセストークン:")
+        self.login_input = QLineEdit()
+        self.login_layout.addWidget(self.login_label)
+        self.login_layout.addWidget(self.login_input)
+
+        self.title_layout = QHBoxLayout()
+        self.title_label = QLabel("タイトル:")
+        self.title_input = QLineEdit()
+        self.title_layout.addWidget(self.title_label)
+        self.title_layout.addWidget(self.title_input)
+
+        self.memo_label = QLabel("メモを入力してください:")
+        self.memo_input = QLineEdit()
+        self.tag_label = QLabel("Tagを入力してください:")
+        self.tag_input = QLineEdit()
+        self.private_checkbox = QCheckBox("非公開（プライベート）にする")
+        self.save_button = QPushButton("保存")
+        self.save_button.clicked.connect(self.save_memo)
+
+        self.memo_list_widget = QListWidget()
+
+        self.get_button = QPushButton("Qiitaに投稿")
+        self.get_button.clicked.connect(self.post_to_qiita)
+
+        layout.addLayout(self.login_layout)
+        layout.addLayout(self.title_layout)
+        layout.addWidget(self.memo_label)
+        layout.addWidget(self.memo_input)
+        layout.addWidget(self.tag_label)
+        layout.addWidget(self.tag_input)
+        layout.addWidget(self.private_checkbox)
+        layout.addWidget(self.save_button)
+        layout.addWidget(self.memo_list_widget)
+        layout.addWidget(self.get_button)
+
+        self.setLayout(layout)
+        self.setWindowTitle("Memo App")
+        self.show()
+
+    def save_memo(self):
+        memo_text = self.memo_input.text()
+        if memo_text:
+            self.memo_list.append(memo_text)
+            tag_text = self.tag_input.text()
+            self.tag_list.append(tag_text)
+            title_text = self.title_input.text()
+            is_private = self.private_checkbox.isChecked()
+            visibility = "private" if is_private else "public"
+            self.memo_list_widget.addItem(f"{title_text}: {memo_text} [Tag: {tag_text}] - {visibility}")
+            self.memo_input.clear()
+            self.tag_input.clear()
+            self.title_input.clear()
+
+    def post_to_qiita(self):
+        selected_items = self.memo_list_widget.selectedItems()
+        if not selected_items:
+            QMessageBox.warning(self, "エラー", "メモを選択してください。")
+            return
+
+        qiita_token = self.login_input.text()
+        if not qiita_token:
+            QMessageBox.warning(self, "エラー", "Qiitaアクセストークンを入力してください。")
+            return
+
+        for item in selected_items:
+            text_with_tag_visibility = item.text()
+            title, memo_text, tag_text, visibility = self.extract_title_memo_tag_and_visibility(text_with_tag_visibility)
+            response = self.create_qiita_post(title, memo_text, tag_text, visibility, qiita_token)
+
+            if response and response.status_code == 201:
+                QMessageBox.information(self, "成功", f"'{title}' をQiitaに投稿しました！")
+            else:
+                QMessageBox.warning(self, "エラー", f"'{title}' の投稿に失敗しました。Qiitaのアクセストークンを確認してください。")
+                print("ステータスコード:", response.status_code)
+                print("レスポンス内容:", response.json())
+
+    def extract_title_memo_tag_and_visibility(self, text_with_tag_visibility):
+        parts = text_with_tag_visibility.split(": ")
+        title = parts[0]
+        text_with_tag_visibility = parts[1]
+        tag_and_visibility = text_with_tag_visibility.split(" [Tag: ")
+        memo_text = tag_and_visibility[0].strip()
+        if len(tag_and_visibility) > 1:
+            tag_visibility = tag_and_visibility[1].split("] - ")
+            tag_text = tag_visibility[0].strip()
+            visibility = tag_visibility[1].strip()
+        else:
+            tag_text = ""
+            visibility = ""
+        return title, memo_text, tag_text, visibility
+
+
+    def create_qiita_post(self, title, text, tag, visibility, qiita_token):
+        qiita_api_url = "https://qiita.com/api/v2/items"
+
+        headers = {
+            "Authorization": f"Bearer {qiita_token}",
+            "Content-Type": "application/json",
+        }
+
+        data = {
+            "title": title,
+            "body": text,
+            "tags": [{"name": tag}] if tag else [],
+            "private": True if visibility == "private" else False,
+            "rendered_body": text,  # 本文をマークダウン方式で出力
+        }
+
+        try:
+            response = requests.post(qiita_api_url, headers=headers, data=json.dumps(data))
+            return response
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return None
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MemoApp()
+    sys.exit(app.exec_())
